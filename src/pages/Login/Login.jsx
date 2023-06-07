69b82df2bc0c7f3e9660451ffa9fb504d5e1bf6a
imransarkerweb <imransarker.web@gmail.com>
2023-06-07 08:53:13 +0600

    login page validation added

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,23 @@
 import { Link } from "react-router-dom";
 import SocialLogin from "../../components/SocialLogin/SocialLogin";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
 
 const Login = () => {
-  const handleLogin = (e) => {
-    e.preventDefault();
+  const [showPassword, setShowPassword] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    // Handle form submission logic here
+    console.log(data);
   };
+
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content flex flex-col md:flex-row">
@@ -17,38 +30,55 @@ const Login = () => {
           </p>
         </div>
         <div className="card md:w-1/2 w-full max-w-sm shadow-2xl bg-base-100">
-          <form onSubmit={handleLogin} className="card-body">
+          <form onSubmit={handleSubmit(onSubmit)} className="card-body">
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
               </label>
               <input
                 type="email"
-                name="email"
-                required
+                {...register("email", { required: "Email is required." })}
                 placeholder="email"
                 className="input input-bordered"
               />
+              {errors.email && (
+                <p role="alert" className="text-red-500">
+                  {errors.email.message}
+                </p>
+              )}
             </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Password</span>
               </label>
-              <input
-                type="password"
-                name="password"
-                required
-                placeholder="password"
-                className="input input-bordered"
-              />
+              <div className="flex items-center">
+                <input
+                  type={showPassword ? "text" : "password"}
+                  {...register("password", {
+                    required: "Password is required.",
+                  })}
+                  placeholder="password"
+                  className="input w-full input-bordered"
+                />
+                <span
+                  className="password-toggle -ml-8 text-2xl"
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  {showPassword ? <FaEye /> : <FaEyeSlash />}
+                </span>
+              </div>
+              {errors.password && (
+                <p role="alert" className="text-red-500">
+                  {errors.password.message}
+                </p>
+              )}
               <label className="label">
-                <a href="#" className="label-text-alt link link-hover">
+                <Link to="#" className="label-text-alt link link-hover">
                   Forgot password?
-                </a>
+                </Link>
               </label>
             </div>
 
-            {/* TODO: make button disabled for captcha later */}
             <div className="form-control mt-6">
               <input
                 disabled={false}
@@ -61,12 +91,12 @@ const Login = () => {
           <p className="text-center mb-4">
             <small>
               New here?{" "}
-              <Link className="text-blue-400 " to="../signup">
+              <Link className="text-blue-400" to="../signup">
                 Create Account.
               </Link>
             </small>
           </p>
-          <SocialLogin></SocialLogin>
+          <SocialLogin />
         </div>
       </div>
     </div>
