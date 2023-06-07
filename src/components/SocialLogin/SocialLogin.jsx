e1ebdb62d6e06d38a22add3e70c0004d00ec9bea
imransarkerweb <imransarker.web@gmail.com>
2023-06-07 10:06:26 +0600

    google sign in implemented

--- src/components/SocialLogin/SocialLogin.jsx
+++ src/components/SocialLogin/SocialLogin.jsx
@@ -1,7 +1,21 @@
 import { FaGoogle } from "react-icons/fa";
+import useAuth from "../../hooks/useAuth";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const SocialLogin = () => {
-  const handleGoogleSignIn = () => {};
+  const { googleSignIn } = useAuth();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location?.state?.from?.pathname || "/";
+  const handleGoogleSignIn = () => {
+    googleSignIn()
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        navigate(from, { replace: true });
+      })
+      .catch((error) => console.log(error.message));
+  };
   return (
     <div>
       <div className="divider"></div>
