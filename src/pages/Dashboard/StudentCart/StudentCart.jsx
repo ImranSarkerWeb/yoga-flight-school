02c4ebc38799fb7f9d8f1db03ba97600b87c81d1
imransarkerweb <imransarker.web@gmail.com>
2023-06-10 19:18:36 +0600

    student cart added

--- src/pages/Dashboard/StudentCart/StudentCart.jsx
+++ src/pages/Dashboard/StudentCart/StudentCart.jsx
@@ -1,5 +1,89 @@
+import Swal from "sweetalert2";
+import useCart from "../../../hooks/useCart";
+import { Link } from "react-router-dom";
+import { FaTrashAlt } from "react-icons/fa";
+
 const StudentCart = () => {
-  return <div>my all selected coursee</div>;
+  const [cart, refetch] = useCart();
+
+  const total = cart.reduce((sum, item) => item.price + sum, 0);
+
+  const handleDelete = (item) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/carts/${item._id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount > 0) {
+              refetch();
+              Swal.fire("Deleted!", "Your file has been deleted.", "success");
+            }
+          });
+      }
+    });
+  };
+
+  return (
+    <div className="w-[80%] mx-auto">
+      <div className=" font-semibold h-[60px] flex justify-evenly items-center">
+        <h3 className="text-3xl">Total Items: {cart.length}</h3>
+        <h3 className="text-3xl">Total Price: ${Math.round(total)}</h3>
+        <Link to="/dashboard/payment">
+          <button className="btn btn-info btn-sm">Pay Now</button>
+        </Link>
+      </div>
+      <div className="overflow-x-auto w-full">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Course</th>
+              <th>Course Name</th>
+              <th className="text-right">Price</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cart.map((item, index) => (
+              <tr key={item._id}>
+                <td>{index + 1}</td>
+                <td>
+                  <div className="avatar">
+                    <div className="mask mask-squircle w-24 h-24">
+                      <img
+                        src={item.image}
+                        alt="Avatar Tailwind CSS Component"
+                      />
+                    </div>
+                  </div>
+                </td>
+                <td>{item.name}</td>
+                <td className="text-end">${item.price}</td>
+                <td>
+                  <button
+                    onClick={() => handleDelete(item)}
+                    className="btn btn-ghost bg-red-600  text-white"
+                  >
+                    <FaTrashAlt></FaTrashAlt>
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default StudentCart;
